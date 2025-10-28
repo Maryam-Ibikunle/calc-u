29caf145859d52c9e37c0a6e5fefbb2f02cc04f8
Maryam Ibikunle <ibikunlemaryamibidoyin@gmail.com>
2025-10-28 13:25:11 +0100

    functional calculator

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const display = document.getElementById('display')
 const number = document.querySelectorAll('.items')
 const equal = document.getElementById('equal')
+const clear = document.getElementById('clear')
+
+display.addEventListener("keydown", e => e.preventDefault());
 number.forEach(num =>{
     num.addEventListener('click', ()=>{
         display.value += num.textContent
     })
 })
 equal.addEventListener('click', ()=>{
-    display.value = eval(String(display.value))
+    try{
+    const answer = Function("return " + String(display.value)
+    )();
+    console.log(answer)
+    display.value = String(answer)
+    equal.disabled = false
+}catch (error){
+    setTimeout(()=>{
+        display.value = ""
+    },2000)
+    display.value = "insert an operation before a bracket"
+}})
+clear.addEventListener('click', ()=>{
+    display.value = ""
 })
