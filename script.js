44907a9e1abf07a4164a78aca7796abbcf30ed9e
Maryam Ibikunle <ibikunlemaryamibidoyin@gmail.com>
2025-10-28 12:38:01 +0100

    Evaluation and display on click are fully functional

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const display = document.getElementById("display");
-function show(){
-    display.value += input;
-}
+const display = document.getElementById('display')
+const number = document.querySelectorAll('.items')
+const equal = document.getElementById('equal')
+number.forEach(num =>{
+    num.addEventListener('click', ()=>{
+        display.value += num.textContent
+    })
+})
+equal.addEventListener('click', ()=>{
+    display.value = eval(String(display.value))
+})
